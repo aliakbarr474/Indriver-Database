cc2b69b7bb1cc1478e0ae7f46308c652b5f3b7bf
fatimaarazaa54 <fatima.raza54@yahoo.com>
2024-06-02 18:14:23 +0500

    Create accessoriesinfo.sql

---
+++ accessoriesinfo.sql
@@ -0,0 +1,17 @@
+SELECT * FROM outfitters.accessories;
+INSERT INTO accessories (name, size, gender, type, price, color, material, quantity) VALUES
+('Sunglasses', 'M', 'Male', 'Eyewear', 2000, 'Navy', 'Plastic', 40),
+('Leather Belt', 'L', 'Male', 'Belt', 4000, 'Brown', 'Leather', 20),
+('Scarf', 'M', 'Female', 'Neckwear', 1000, 'Pink', 'Silk', 40),
+('Cap', 'S', 'Male', 'Headwear', 1000, 'Orange', 'Cotton', 30),
+('Bracelet', 'M', 'Female', 'Jewelry', 1500, 'Gold', 'Metal', 40),
+('Earrings', 'S', 'Female', 'Jewelry', 2000, 'Silver', 'Metal', 60),
+('Wallet', 'M', 'Male', 'Wallet', 2000, 'Brown', 'Leather', 30),
+('Necklace', 'L', 'Female', 'Jewelry', 3000, 'White', 'Pearl', 15),
+('Watch', 'S', 'Male', 'Watch', 4000, 'Silver', 'Metal', 10),
+('Gloves', 'M', 'Female', 'Handwear', 3000, 'Red', 'Wool', 30),
+('Backpack', 'L', 'Male', 'Bag', 4000, 'Blue', 'Canvas', 30),
+('Ring', 'S', 'Female', 'Jewelry', 1000, 'Gold', 'Metal', 40),
+('Tie', 'M', 'Male', 'Neckwear', 1500, 'Blue', 'Silk', 30),
+('Beanie', 'L', 'Male', 'Headwear', 1000, 'Grey', 'Wool', 45),
+('Keychain', 'S', 'Female', 'Keychain', 500, 'Silver', 'Metal', 60);
