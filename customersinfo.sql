e116d20f1415312649ea28992ea56b137552c55e
fatimaarazaa54 <fatima.raza54@yahoo.com>
2024-06-02 18:15:44 +0500

    Create customersinfo.sql

---
+++ customersinfo.sql
@@ -0,0 +1,18 @@
+SELECT * FROM outfitters.customers;
+INSERT INTO customers (first_name, last_name, phone_number, gender, age, address) 
+VALUES 
+('Ali', 'Akbar', '0334567890', 'Male', 30, 'LUMS, Lahore, Pakistan'), 
+('Rida', 'Rana', '0335234678', 'Female', 18, 'Bath Island, Karachi, Pakistan'), 
+('Zain', 'Pasha', '0336782345', 'Male', 20, 'F6, Islamabad, Pakistan'), 
+('Ayesha', 'Ishaque', '0332657893', 'Female', 21, 'Gt road, Faisalabad, Pakistan'), 
+('Bilal', 'Hassni', '0332134544', 'Male', 25, '202 Lane, Multan, Pakistan'), 
+('Fatima', 'Raza', '03376543211', 'Female', 27, 'University of peshawar, Peshawar, Pakistan'), 
+('Hamza', 'Waqar', '0338651222', 'Male', 32, 'Satellite Town, Quetta, Pakistan'), 
+('Mariam', 'Nawaz', '03224455667', 'Female', 22, '505 Avenue, Gujranwala, Pakistan'), 
+('Omar', 'Tareen', '0336677237', 'Male', 29, 'Jinnah Town, Quetta, Pakistan'), 
+('Saba', 'Riaz', '0333444555', 'Female', 23, 'Clifton,Karachi, Pakistan'), 
+('Sana', 'Ali', '033445566', 'Female', 31, 'Highway, Hyderabad, Pakistan'), 
+('Uneeza', 'Hakim', '0345223344', 'Female', 23, 'Raya, Lahore, Pakistan'), 
+('Salaar', 'Ahmed', '03336666677', 'Male', 23, 'G-6, Islamabad, Pakistan'), 
+('Hina', 'Aslam', '03335555566', 'Female', 19, 'Nust, Quetta, Pakistan'), 
+('Ahmed', 'Yousaf', '0332222223', 'Male', 20, 'Gulshan-e-iqbal,Karachi, Pakistan');
