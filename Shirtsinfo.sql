beafa04f4c7b46660b301bc051c287fdac652bc0
fatimaarazaa54 <fatima.raza54@yahoo.com>
2024-06-02 18:13:18 +0500

    Create Shirtsinfo.sql

---
+++ Shirtsinfo.sql
@@ -0,0 +1,17 @@
+SELECT * FROM outfitters.shirts;
+INSERT INTO shirts (name, size, gender, type, price, color, material, quantity) VALUES
+('Slim Fit Shirt', 'M', 'Male', 'Casual', 2000, 'Blue', 'Cotton', 15),
+('Formal Shirt', 'L', 'Male', 'Formal', 2500, 'White', 'Cotton', 30),
+('Polo Shirt', 'S', 'Male', 'Polo', 1500, 'Pink', 'Cotton', 25),
+('Dress Shirt', 'M', 'Male', 'Dress', 4000, 'Black', 'Cotton', 40),
+('T-Shirt', 'L', 'Female', 'Tee', 1000, 'Blue', 'Cotton', 60),
+('Blouse', 'S', 'Female', 'Casual',2000 , 'Navy', 'Silk', 50),
+('Tank Top', 'M', 'Female', 'Tank', 1000, 'Grey', 'Cotton', 60),
+('Tank Top', 'L', 'Male', 'Tank', 2000, 'Black', 'Cotton', 40),
+('Button-up', 'M', 'Female', 'Casual', 4000, 'Blue', 'Linen', 35),
+('Halter- Top', 'S', 'Female', 'Casual', 1000, 'White', 'Cotton', 45),
+('Hoodie', 'M', 'Male', 'Hoodie', 3000, 'Purple', 'Fleece', 15),
+('Sweater', 'L', 'Female', 'Sweater', 4000, 'Red', 'Wool', 30),
+('Flannel', 'S', 'Male', 'Casual', 2000, 'Red', 'Cotton', 50),
+('V-Neck', 'M', 'Female', 'Tee', 1400, 'Yellow', 'Cotton', 70),
+('Dress Shirt', 'L', 'Female', 'Dress', 5000, 'Orange', 'Linen', 40);
