14f4d3ab4ac675916aab1d2b93aa57ce8a276577
fatimaarazaa54 <fatima.raza54@yahoo.com>
2024-06-02 18:12:30 +0500

    Create shoesinfo.sql

---
+++ shoesinfo.sql
@@ -0,0 +1,17 @@
+SELECT * FROM outfitters.bottom_wear;
+INSERT INTO shoes (name, size, gender, price, color, material, quantity) VALUES
+('Sneakers', '34', 'Male', 6000, 'White', 'Fabric', 15),
+('Running Shoes', '32', 'Female', 4000, 'Green', 'Mesh', 20),
+('Loafers', '40', 'Male', 7000, 'Black', 'Suede', 40),
+('Sandals', '32', 'Female', 4000, 'Gold', 'Leather', 40),
+('Flip Flops', '37', 'Male', 1500, 'Yellow', 'Rubber', 50),
+('Ankle Boots', '37', 'Female', 4500, 'Brown', 'Leather', 24),
+('Sneakers', '38', 'Female', 6000, 'Purple', 'Synthetic', 35),
+('Mules', '36', 'Female', 4000, 'White', 'Synthetic', 40),
+('Dress Shoes', '44', 'Male', 6000, 'Brown', 'Leather', 15),
+('Flats', '32', 'Female', 4000, 'Red', 'Leather', 55),
+('Hiking Boots', '43', 'Male', 7000, 'Brown', 'Leather', 45),
+('Wedges', '36', 'Female', 3300, 'Grey', 'Suede', 30),
+('Slip-ons', '41', 'Male', 2800, 'Navy', 'Canvas', 40),
+('Heels', '38', 'Female', 6000, 'Silver', 'Synthetic', 15),
+('Boots', '44', 'Male', 4800, 'black', 'Leather', 20);
